054dcc0017221f0aa499c8cb0ba6b24aea0e57ad
MunizNeto84 <muniznetto@outlook.com>
2024-02-18 13:31:28 -0300

    criando `a animacao do passaro`

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ function barrierPair(height, space, x) {
 //const barrier = new barrierPair(43.7, 12.5, 25);
 //document.querySelector("[flappy]").appendChild(barrier.element);
 
-function Barriers(height, width, space, gap, point) {
+function Barriers(height, width, space, gap, score) {
   this.pairs = [
     new barrierPair(height, space, width),
     new barrierPair(height, space, width + gap),
@@ -66,16 +66,46 @@ function Barriers(height, width, space, gap, point) {
       const split = width / 2;
       const goal = pair.getX() + displacement >= split && pair.getX() < split;
       if (goal) {
-        point;
+        score;
       }
     });
   };
 }
 
-//test
+function Bird(gameHeight) {
+  let fly = false;
+  this.element = newElement("img", "bird");
+  this.element.src = "./flappy.svg";
+
+  this.getY = () => parseInt(this.element.style.bottom.split("px")[0]);
+  this.setY = (y) => (this.element.style.bottom = `${y}px`);
+  window.onkeydown = (e) => (fly = true);
+  window.onkeyup = (e) => (fly = false);
+
+  this.animation = () => {
+    const newY = this.getY() + (fly ? 8 : -5);
+    const maxHeight = gameHeight - this.element.clientHeight;
+
+    if (newY <= 0) {
+      this.setY(0);
+    } else if (newY >= maxHeight) {
+      this.setY(maxHeight);
+    } else {
+      this.setY(newY);
+    }
+  };
+  this.setY(gameHeight / 2);
+}
+
+/*test
 const barriers = new Barriers(700, 1200, 200, 400);
+const bird = new Bird(700);
 const area = document.querySelector("[flappy]");
+
+area.appendChild(bird.element);
 barriers.pairs.forEach((pair) => area.appendChild(pair.element));
 setInterval(() => {
   barriers.animation();
+  bird.animation();
 }, 20);
+*/
